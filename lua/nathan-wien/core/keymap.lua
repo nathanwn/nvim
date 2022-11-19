ed4e511b51ffdbbd29e7a5ffefed31430ecf3347
nathan-wien <49882031+nathan-wien@users.noreply.github.com>
2022-11-19 11:45:22 +1000

    fix: remove unnecessary keymaps

--- lua/nathan-wien/core/keymap.lua
+++ lua/nathan-wien/core/keymap.lua
@@ -20,12 +20,6 @@ vim.keymap.set("n", "<C-s><C-l>", ":vertical resize +1<CR>")
 vim.keymap.set("n", "<C-s>h", "<C-w>t<C-w>K")
 vim.keymap.set("n", "<C-s>k", "<C-w>t<C-w>H")
 
--- Remap for word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-vim.keymap.set("n", "0", "v:count == 0 ? 'g0' : '0'", { expr = true, silent = true })
-vim.keymap.set("n", "$", "v:count == 0 ? 'g$' : '$'", { expr = true, silent = true })
-
 -- Quickfix list
 vim.keymap.set("n", "[q", "<cmd>cprevious<CR>")
 vim.keymap.set("n", "]q", "<cmd>cnext<CR>")
@@ -34,8 +28,7 @@ vim.keymap.set("n", "]q", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
--- Smart dd. Credit:
--- https://www.reddit.com/r/neovim/comments/w0jzzv/comment/igfjx5y/?utm_source=share&utm_medium=web2x&context=3
+-- Smart dd. Credit: https://www.reddit.com/r/neovim/comments/w0jzzv/comment/igfjx5y
 vim.keymap.set("n", "dd", function()
   if vim.api.nvim_get_current_line():match("^%s*$") then
     return '"_dd'
