5bd4fcdf0d078787981acd46962c247d21a3b584
nathan-wien <49882031+nathan-wien@users.noreply.github.com>
2022-11-28 06:30:22 +1000

    perf: optimize performance

--- after/plugin/metals.lua
+++ after/plugin/metals.lua
@@ -1,13 +1,11 @@
-if not pcall(require, "metals") then
-  return
-end
-
-local metals = require("metals")
-local config = require("nathan-wien.lsp.servers.metals")
-
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "scala", "sbt" },
   callback = function()
+    if not pcall(require, "metals") then
+      return
+    end
+    local metals = require("metals")
+    local config = require("nathan-wien.lsp.servers.metals")
     metals.initialize_or_attach(config)
     -- Getting error messages for nvim-metals
     vim.opt_local.shortmess:remove("F")
