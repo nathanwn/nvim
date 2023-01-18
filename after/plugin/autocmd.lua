21c7d028214ea12e25efc7690d64ea5413365369
nathan-wien <49882031+nathan-wien@users.noreply.github.com>
2023-01-18 20:33:30 +1000

    feat: enable removing trailing whitespaces on save

--- after/plugin/autocmd.lua
+++ after/plugin/autocmd.lua
@@ -1,5 +1,4 @@
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*.tex" },
   command = [[%s/\s\+$//e]],
   group = vim.api.nvim_create_augroup("RemoveTrailingWhitespaces", { clear = true }),
 })
