d91a57aa0f14be75287f4e750373e85a8404bcd0
nathanwn <49882031+nathanwn@users.noreply.github.com>
2023-08-06 14:40:59 +1000

    feat: truncate branch name if too long

--- lua/nathan-wien/plugins/heirline/components/branch.lua
+++ lua/nathan-wien/plugins/heirline/components/branch.lua
@@ -4,10 +4,19 @@ local conditions = require("heirline.conditions")
 local branch = {
   condition = conditions.is_git_repo,
   init = function(self)
-    self.branch = vim.b.gitsigns_status_dict.head or "[no branch]"
+    if vim.b.gitsigns_status_dict then
+      self.branch = vim.b.gitsigns_status_dict.head or "[no branch]"
+    end
   end,
   provider = function(self)
-    return string.format(" %s ", self.branch)
+    local branch_label
+    local limit = 15
+    if string.len(self.branch) > limit then
+      branch_label = string.format("%s...", string.sub(self.branch, 1, limit))
+    else
+      branch_label = self.branch
+    end
+    return string.format(" %s ", branch_label)
   end,
   hl = {
     fg = utils.get_highlight("HeirlineBranch").fg,
