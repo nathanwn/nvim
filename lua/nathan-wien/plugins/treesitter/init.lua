41b93c60e531109058ec0e18870f0c8fea4fb80b
nathanwn <49882031+nathanwn@users.noreply.github.com>
2023-10-29 11:07:45 +1000

    fix(treesitter): pin exact commit

--- lua/nathan-wien/plugins/treesitter/init.lua
+++ lua/nathan-wien/plugins/treesitter/init.lua
@@ -1,6 +1,8 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    -- commit = "7be9ebd06abe2ed836abd3e91acb0b243ddcafba",
+    version = "v0.9.1",
     build = ":TSUpdate",
     config = require("nathan-wien.plugins.treesitter.config.treesitter"),
   },
