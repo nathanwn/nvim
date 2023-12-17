344df621e3e9cb02c11ee7e55fdb8809f6517359
nathanwn <49882031+nathanwn@users.noreply.github.com>
2023-12-17 12:13:45 +1000

    chore: reformat

--- lua/nathan-wien/plugins/lsp/servers/lua_ls.lua
+++ lua/nathan-wien/plugins/lsp/servers/lua_ls.lua
@@ -48,13 +48,13 @@ return {
         checkThirdParty = "Disable",
         -- Make the server aware of Neovim runtime files
         library = {
-          vim.env.VIMRUNTIME
+          vim.env.VIMRUNTIME,
           -- "${3rd}/luv/library"
           -- "${3rd}/busted/library",
-        }
+        },
         -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
         -- library = vim.api.nvim_get_runtime_file("", true)
-      }
+      },
     },
   },
 }
