95a84ce9528f4886094c85f0a9e83f428c208450
nathanwn <49882031+nathanwn@users.noreply.github.com>
2024-04-12 14:07:30 +1000

    chore: clean up autocmd

--- after/plugin/autocmd.lua
+++ after/plugin/autocmd.lua
@@ -1,3 +1,17 @@
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+--   desc = 'Highlight when yanking (copying) text',
+--   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+--   callback = function()
+--     vim.highlight.on_yank()
+--   end,
+-- })
+
+-- Remove trailing whitespaces
 -- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 --   command = [[%s/\s\+$//e]],
 --   group = vim.api.nvim_create_augroup("RemoveTrailingWhitespaces", { clear = true }),
