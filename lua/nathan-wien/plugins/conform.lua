509b140ba55f08149a61e37f06437ab4ad192c2a
nathanwn <49882031+nathanwn@users.noreply.github.com>
2024-04-19 22:12:01 +1000

    feat: enable auto-install formatters and linters

--- lua/nathan-wien/plugins/conform.lua
+++ lua/nathan-wien/plugins/conform.lua
@@ -2,7 +2,11 @@ local formatters_by_ft = {
   lua = { "stylua" },
   rust = { "rustfmt" },
   -- Conform will run multiple formatters sequentially
-  -- python = { "isort", "black" },
+  python = {
+    "autoflake",
+    "isort",
+    "black",
+  },
   -- Use a sub-list to run only the first available formatter
   javascript = { { "prettierd", "prettier" } },
   typescript = { { "prettierd", "prettier" } },
@@ -16,18 +20,34 @@ local function format()
   end)
   if vim.tbl_get(formatters_by_ft, filetype) ~= nil then
     require("conform").format({ bufnr = 0 })
-  elseif filetype == "python" then
-    vim.cmd("silent !pre-commit run black --files %")
-    vim.cmd("silent !pre-commit run isort --files %")
+    -- elseif filetype == "python" then
+    --   vim.cmd("silent !pre-commit run black --files %")
+    --   vim.cmd("silent !pre-commit run isort --files %")
   end
 end
 
 return {
   "stevearc/conform.nvim",
-  version = "v5.3.0",
+  version = "v5.6.0",
   config = function()
     require("conform").setup({
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        return {
+          timeout_ms = 3000,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
       formatters_by_ft = formatters_by_ft,
+      formatters = {
+        autoflake = {
+          prepend_args = { "--remove-all-unused-imports" },
+        },
+      },
     })
     vim.api.nvim_create_user_command("F", format, {})
   end,
--- lua/nathan-wien/plugins/mason.lua
+++ lua/nathan-wien/plugins/mason.lua
@@ -28,4 +28,28 @@ return {
       })
     end,
   },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+      },
+    },
+    opts = {
+      ensure_installed = {
+        -- you can pin a tool to a particular version
+        -- { 'golangci-lint', version = 'v1.47.0' },
+        -- you can turn off/on auto_update per tool
+        -- { 'bash-language-server', auto_update = true },
+        -- lua
+        "stylua",
+        -- python
+        "autoflake",
+        "black",
+        "isort",
+        -- shell
+        "shellcheck",
+      },
+    },
+  },
 }
