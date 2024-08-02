0ee6baaf210503087cae5176f13233b6a7937618
nathanwn <49882031+nathanwn@users.noreply.github.com>
2024-08-03 00:56:39 +1000

    fix: disable jdtls formatting

--- lua/nathan-wien/plugins/conform.lua
+++ lua/nathan-wien/plugins/conform.lua
@@ -36,7 +36,7 @@ return {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, java = true }
         return {
           timeout_ms = 3000,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
