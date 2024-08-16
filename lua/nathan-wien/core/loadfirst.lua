089ecd9e3e0c9058e35f9e75f66fdbb4e465af01
nathanwn <49882031+nathanwn@users.noreply.github.com>
2024-08-16 12:51:33 +1000

    refactor: use new nvim api `vim.keycode`

--- lua/nathan-wien/core/loadfirst.lua
+++ lua/nathan-wien/core/loadfirst.lua
@@ -1,15 +1,4 @@
 vim.g.python3_host_prog = require("nathan-wien.utils").python_venv .. "/bin/python3"
 
--- Concise way to escape termcodes
-local function t(str)
-  -- Adjust boolean arguments as needed
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-vim.g.mapleader = t("<Space>")
-vim.g.maplocalleader = t("<Space>")
-
--- vim.env.PATH = vim.env.PATH
--- .. ":"
--- .. vim.fn.stdpath("data")
--- .. "/extra-npm-dependencies/node_modules/.bin"
+vim.g.mapleader = vim.keycode("<Space>")
+vim.g.maplocalleader = vim.keycode("<Space>")
