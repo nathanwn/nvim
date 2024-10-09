e11e39cd375ce1223d875bf4f95b830ca1ce81cd
nathanwn <49882031+nathanwn@users.noreply.github.com>
2024-10-09 15:13:09 +1000

    use ccc for color highlighting

--- lua/nathan-wien/plugins/misc.lua
+++ lua/nathan-wien/plugins/misc.lua
@@ -2,9 +2,26 @@ return {
   -- Core plugins
   { "nvim-lua/popup.nvim" },
   { "nvim-lua/plenary.nvim" },
-  -- Colorizer
-  { "chrisbra/Colorizer" },
   -- Grammar checker
+  {
+    "uga-rosa/ccc.nvim",
+    config = function()
+      require("ccc").setup({
+        highlighter = {
+          auto_enable = true,
+          lsp = true,
+        },
+      })
+    end,
+    keys = {
+      {
+        "<Leader>tc",
+        "<cmd>CccHighlighterToggle<CR>",
+        mode = "n",
+        desc = "Toggle color highlighter",
+      },
+    },
+  },
   { "rhysd/vim-grammarous" },
   -- Startup time
   { "dstein64/vim-startuptime" },
