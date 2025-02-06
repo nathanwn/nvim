45c2dfc9b3d32f42a5995670ba04fae02e896054
nathanwn <49882031+nathanwn@users.noreply.github.com>
2025-02-06 18:15:53 +0700

    fix bug causing nvim 0.10 to crash randomly on macos intel

--- lua/nathan-wien/core/loadfirst.lua
+++ lua/nathan-wien/core/loadfirst.lua
@@ -1,4 +1,7 @@
-vim.g.python3_host_prog = require("nathan-wien.utils").python_venv .. "/bin/python3"
+-- NOTE: if the virtual environment does not exist, this will crash
+-- nvim on MacOS.
+-- TODO: figure out how to create this virtual environment automagically
+-- vim.g.python3_host_prog = require("nathan-wien.utils").python_venv .. "/bin/python3"
 
 vim.g.mapleader = vim.keycode("<Space>")
 vim.g.maplocalleader = vim.keycode("<Space>")
