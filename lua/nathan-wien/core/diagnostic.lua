0b6af3e2c952c14d65580789123a0a61c3a745cd
nathanwn <49882031+nathanwn@users.noreply.github.com>
2025-04-09 00:40:12 +1000

    more nvim 0.11

--- lua/nathan-wien/core/diagnostic.lua
+++ lua/nathan-wien/core/diagnostic.lua
@@ -1,8 +1,3 @@
--- Virtual text
--- coloring
--- Read: https://neovim.io/doc/user/lsp.html
--- vim.cmd([[ hi LspDiagnosticsDefaultHint guifg='#A0A0A0' ]])
--- disable
 vim.diagnostic.config({
   virtual_text = {
     severity = {
@@ -18,17 +13,3 @@ vim.diagnostic.config({
     severity_sort = true,
   },
 })
-
--- Diagnostic Signs
--- Read: https://neovim.io/doc/user/diagnostic.html
-local signs = {
-  ["Error"] = "E", -- "",
-  ["Info"] = "I", -- "",
-  ["Hint"] = "H", --"",
-  ["Warn"] = "W", --"",
-}
-for kind, icon in pairs(signs) do
-  local name = "DiagnosticSign" .. kind
-  local texthl_group = "DiagnosticSign" .. kind
-  vim.fn.sign_define(name, { text = icon, texthl = texthl_group })
-end
