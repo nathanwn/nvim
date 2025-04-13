b77fad5c3b8e1ee1e459929f8bf92076dc1f57a8
nathanwn <49882031+nathanwn@users.noreply.github.com>
2025-04-13 18:45:05 +1000

    use snacks for file handling niceties

---
+++ lua/nathan-wien/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    quickfile = { enabled = true },
+  },
+}
