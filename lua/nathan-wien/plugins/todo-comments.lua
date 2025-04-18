3c44faf7354cbaeaa2ea6988ecb6ecccdaf18a7b
nathanwn <49882031+nathanwn@users.noreply.github.com>
2025-04-18 17:32:56 +1000

    properly configure todo-comment for TODO(username) pattern

--- lua/nathan-wien/plugins/todo-comments.lua
+++ lua/nathan-wien/plugins/todo-comments.lua
@@ -2,4 +2,12 @@ return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   tag = "v1.4.0",
+  event = "VimEnter",
+  opts = {
+    signs = false,
+    -- Highlight the TODO(username) pattern
+    -- See https://github.com/folke/todo-comments.nvim/issues/10#issuecomment-2446101986.
+    search = { pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]] },
+    highlight = { pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]] },
+  },
 }
