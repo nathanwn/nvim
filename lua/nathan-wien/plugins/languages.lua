2a220fc656c935e29a459a2b6e6333a197a352f3
nathanwn <49882031+nathanwn@users.noreply.github.com>
2025-06-13 23:38:57 +1000

    Clean up unused plugins

--- lua/nathan-wien/plugins/languages.lua
+++ lua/nathan-wien/plugins/languages.lua
@@ -4,27 +4,4 @@ return {
     "lervag/vimtex",
     tag = "v2.15",
   },
-  -- ANTLR4
-  {
-    "dylon/vim-antlr",
-    ft = { "antlr4" },
-  },
-  -- Lark
-  {
-    "lark-parser/vim-lark-syntax",
-    ft = { "lark" },
-  },
-  -- Markdown
-  -- {
-  --   "iamcco/markdown-preview.nvim",
-  --   version = "v0.0.10",
-  -- },
-  -- Emmet
-  -- { "mattn/emmet-vim" }
-  -- Pandoc
-  -- {
-  --   "vim-pandoc/vim-pandoc",
-  --   dependencies = { "vim-pandoc/vim-pandoc-syntax" },
-  --   ft = { "pandoc" },
-  -- }
 }
